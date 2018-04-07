4f91e7d3fc8c85e8e222b240a29329e3995d03eb
Anduin2017 <Anduin@aiursoft.com>
2018-04-07 16:49:43 +0800

    User info validation

--- src/app/Controllers/userDetail.component.ts
+++ src/app/Controllers/userDetail.component.ts
@@ -8,7 +8,8 @@ import { Conversation } from '../Models/Conversation';
 import { CacheService } from '../Services/CacheService';
 import { Location } from '@angular/common';
 import { Values } from '../values';
-
+import { AiurProtocal } from '../Models/AiurProtocal';
+import { AiurCollection } from '../Models/AiurCollection';
 @Component({
   templateUrl: '../Views/userDetail.html',
   styleUrls: [
@@ -21,6 +22,7 @@ export class UserDetailComponent implements OnInit {
   public user: KahlaUser;
   constructor(
     private apiService: ApiService,
+    private router: Router
   ) { }
 
   public ngOnInit(): void {
@@ -30,7 +32,13 @@ export class UserDetailComponent implements OnInit {
   }
   public save() {
     this.apiService.UpdateInfo(this.user.nickName, this.user.bio).subscribe((t) => {
-      alert(t.message);
+      if (t.code === 0) {
+        this.router.navigate(['/kahla/settings']);
+      } else if ( t.code === -10 ) {
+        swal( t.message, (t as AiurProtocal as AiurCollection<string>).items[0], 'error' );
+      } else {
+        swal('input in failed', t.message, 'error');
+      }
     });
   }
 }
