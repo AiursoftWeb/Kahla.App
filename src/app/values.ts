6f13f2f53c23de6f66636aad080994332ea9e61e
Anduin2017 <Anduin@aiursoft.com>
2018-04-11 09:15:37 +0800

    Upgrade value to fit latest api level

--- src/app/values.ts
+++ src/app/values.ts
@@ -1,4 +1,4 @@
 export class Values {
     public static serverAddress = 'https://kahla.server.aiursoft.com';
-    public static currentVersion = `0.7.0`;
+    public static currentVersion = `0.8.0`;
 }
