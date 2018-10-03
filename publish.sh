08532887d7705702dbf976fb3266928927456e66
Anduin2017 <Anduin@aiursoft.com>
2018-10-03 22:42:30 +0800

    Simplify configuration.

--- publish.sh
+++ publish.sh
@@ -2,25 +2,8 @@
 
 rm -rf ./www/
 rm -rf ./dist/
-npm run prebuild
-ng build --base-href ./ --prod
-mv ./www/electron.js ./www/index.js
-cp ./package.json ./www
-
-if [[ "$OSTYPE" == "darwin"* ]]
-then
-    sed -i '' '/postinstall/d' ./www/package.json
-else
-    sed -i '/postinstall/d' ./www/package.json
-fi
-
-cd ./www
-
-if ! [ -x "$(command -v electron-builder)" ]
-then
-    ../node_modules/.bin/electron-builder
-else
-    electron-builder
-fi
-
+npm run prod-electron
+cd ./www/
+npm init -y
 cd ..
+./node_modules/.bin/electron-builder
