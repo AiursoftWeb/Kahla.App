35e6abc5a637670060bfe965e8cfdff32e68fc13
xxyzz <elephantpolar@gmail.com>
2018-10-03 21:34:38 +0800

    add icons, build app for macOS

--- publish.sh
+++ publish.sh
@@ -1,10 +1,26 @@
-rm ./www/ -rf
-rm ./dist/ -rf
+#!/bin/bash
+
+rm -rf ./www/
+rm -rf ./dist/
 npm run prebuild
 ng build --base-href ./ --prod
 mv ./www/electron.js ./www/index.js
 cp ./package.json ./www
-sed -i '/postinstall/d' ./www/package.json
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+    sed -i '' '/postinstall/d' ./www/package.json
+else
+    sed -i '/postinstall/d' ./www/package.json
+fi
+
 cd ./www
-electron-builder
+
+if ! [ -x "$(command -v electron-builder)" ]
+then
+    ../node_modules/.bin/electron-builder
+else
+    electron-builder
+fi
+
 cd ..
