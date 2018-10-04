3854d646b86e68046c97123a2f245f4e9c31ae1c
xxyzz <elephantpolar@gmail.com>
2018-10-04 14:37:44 +0800

    fix publish.sh

--- publish.sh
+++ publish.sh
@@ -4,7 +4,14 @@ rm -rf ./www/
 rm -rf ./dist/
 npm run prod-electron
 cp ./package.json ./www/
-sed  -i'' '6,999d' ./www/package.json
-echo '"main": "index.js"' >> ./www/package.json
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+    sed -i '' '6,$d' ./www/package.json
+else
+    sed -i '6,$d' ./www/package.json
+fi
+
+echo '  "main": "index.js"' >> ./www/package.json
 echo '}' >> ./www/package.json
 ./node_modules/.bin/electron-builder
