78aa67d8eb417bc451a86ad1c27b3cf85ca50484
xxyzz <xxyzz@aiursoft.com>
2018-10-23 11:04:33 +0800

    remove branch info from about page

--- src/app/values.ts
+++ src/app/values.ts
@@ -2,4 +2,5 @@ export class Values {
     public static serverAddress = 'https://kahla.server.aiursoft.com';
     public static fileAddress = 'https://oss.aiursoft.com/download/fromkey/';
     public static loadingImgURL = 'https://cdn.aiursoft.com/images/loading.gif';
+    public static sourceCodeURL = 'https://aiursoft.visualstudio.com/Kahla/_git/Kahla.App/commit/';
 }
