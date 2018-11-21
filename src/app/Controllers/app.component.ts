9464988e27adbaf5f978b2c85420c83237b257cf
xxyzz <elephantpolar@gmail.com>
2018-11-21 08:32:50 +0800

    close Swal dialog when history changes

--- src/app/Controllers/app.component.ts
+++ src/app/Controllers/app.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnDestroy } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { InitService } from '../Services/InitService';
+import { PlatformLocation } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-kahla',
     templateUrl: '../Views/app.html',
     styleUrls: ['../Styles/app.css']
 })
+
+
 export class AppComponent implements OnInit, OnDestroy {
     constructor(
-        public initService: InitService) {
+        public initService: InitService,
+        public location: PlatformLocation) {
+            location.onPopState(() => {
+                Swal.close();
+            });
     }
 
     public ngOnInit(): void {
