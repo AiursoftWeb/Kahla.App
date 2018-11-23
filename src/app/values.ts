349b10a219ed9c57a271a601464bf266750ac7bf
xxyzz <elephantpolar@gmail.com>
2018-11-23 10:50:19 +0800

    remove duplicate value

--- src/app/values.ts
+++ src/app/values.ts
@@ -2,5 +2,4 @@ export class Values {
     public static fileAddress = 'https://oss.aiursoft.com/download/fromkey/';
     public static loadingImgURL = 'https://cdn.aiursoft.com/images/loading.gif';
     public static sourceCodeURL = 'https://github.com/AiursoftWeb/Kahla.App/commit/';
-    public static ossDownloadPath = 'https://oss.aiursoft.com/Download/FromKey/';
 }
