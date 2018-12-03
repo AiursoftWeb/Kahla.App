f88e3ab697be501a196b330789a64051104e0597
Anduin2017 <anduin@aiursoft.com>
2018-12-03 12:23:23 +0800

    Borderless electron

--- src/index.js
+++ src/index.js
@@ -10,85 +10,93 @@ let mainWindow
 app.showExitNotif = true
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 512, height: 768, icon :  __dirname + '/assets/48x48.png' })
+    mainWindow = new BrowserWindow(
+        {
+            width: 512,
+            height: 768,
+            icon: __dirname + '/assets/48x48.png',
+            frame: false,
+            minWidth: 200,
+            minHeight: 300
+        })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-  //mainWindow.webContents.openDevTools();
-  mainWindow.on('close', function (event) {
-    if (!app.isQuiting && app.showExitNotif) {
-      event.preventDefault()
-      app.showExitNotif = false
-      new Notification({
-        title: 'Kahla',
-        body: 'Kahla is running in the background.'
-      }).show()
-      mainWindow.hide()
-    }
-    if (!app.isQuiting && !app.shouldQuit) {
-      event.preventDefault()
-      mainWindow.hide()
-    }
-  });
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+    //mainWindow.webContents.openDevTools();
+    mainWindow.on('close', function (event) {
+        if (!app.isQuiting && app.showExitNotif) {
+            event.preventDefault()
+            app.showExitNotif = false
+            new Notification({
+                title: 'Kahla',
+                body: 'Kahla is running in the background.'
+            }).show()
+            mainWindow.hide()
+        }
+        if (!app.isQuiting && !app.shouldQuit) {
+            event.preventDefault()
+            mainWindow.hide()
+        }
+    });
 
-  mainWindow.on('minimize', function (event) {
-    event.preventDefault()
-    mainWindow.hide();
-  });
+    mainWindow.on('minimize', function (event) {
+        event.preventDefault()
+        mainWindow.hide();
+    });
 
 }
 
-var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
-  if (mainWindow) {
-    if (mainWindow.isMinimized()) mainWindow.restore();
-    mainWindow.show();
-    mainWindow.focus();
-  }
+var shouldQuit = app.makeSingleInstance(function (commandLine, workingDirectory) {
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.show();
+        mainWindow.focus();
+    }
 });
 
 if (shouldQuit) {
-  app.quit();
-  return;
+    app.quit();
+    return;
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
-  // mainWindow.webContents.openDevTools();
+    if (mainWindow === null) {
+        createWindow()
+    }
+    // mainWindow.webContents.openDevTools();
 })
 
 let tray = null
 const platform = require('os').platform()
 app.on('ready', () => {
-  if (platform === 'darwin') {
-    tray = new Tray(__dirname + '/assets/KahlaTemplate.png')
-  } else {
-    tray = new Tray(__dirname + '/assets/48x48.png')
-  }
-  tray.addListener('double-click',function () { mainWindow.show(); })
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Kahla', click: function () { mainWindow.show(); }
-    },
-    { type: 'separator'},
-    {
-      label: 'Exit', click: function () { app.isQuiting = true; app.quit(); }
+    if (platform === 'darwin') {
+        tray = new Tray(__dirname + '/assets/KahlaTemplate.png')
+    } else {
+        tray = new Tray(__dirname + '/assets/48x48.png')
     }
-  ])
-  Menu.setApplicationMenu(null)
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
+    tray.addListener('double-click', function () { mainWindow.show(); })
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Kahla', click: function () { mainWindow.show(); }
+        },
+        { type: 'separator' },
+        {
+            label: 'Exit', click: function () { app.isQuiting = true; app.quit(); }
+        }
+    ])
+    Menu.setApplicationMenu(null)
+    tray.setToolTip('This is my application.')
+    tray.setContextMenu(contextMenu)
 })
