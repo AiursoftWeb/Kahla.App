6b2a7efe347776b059ff65827dbdfe09167bbeb4
xxyzz <elephantpolar@gmail.com>
2018-12-11 19:43:47 +0800

    auto save drafts

--- src/app/Controllers/talking.component.ts
+++ src/app/Controllers/talking.component.ts
@@ -29,6 +29,8 @@ export class TalkingComponent implements OnInit, OnDestroy {
         'coral', 'cornflowerblue', 'darkcyan', 'darkgoldenrod'];
     public users = new Map();
     public fileAddress = Values.fileAddress;
+    private conversationID = 0;
+    public autoSaveInterval;
 
     @ViewChild('mainList') public mainList: ElementRef;
     @ViewChild('imageInput') public imageInput;
@@ -68,14 +70,13 @@ export class TalkingComponent implements OnInit, OnDestroy {
     public ngOnInit(): void {
         this.uploadService.talkingDestroied = false;
         this.messageService.updateMaxImageWidth();
-        let conversationID = 0;
         this.headerService.title = 'Loading...';
         this.headerService.returnButton = true;
         this.route.params
             .pipe(
                 switchMap((params: Params) => {
-                    conversationID = params['id'];
-                    return this.conversationApiService.ConversationDetail(conversationID);
+                    this.conversationID = params['id'];
+                    return this.conversationApiService.ConversationDetail(this.conversationID);
                 }),
                 map(t => t.value)
             )
@@ -89,7 +90,7 @@ export class TalkingComponent implements OnInit, OnDestroy {
                     }
                     this.messageService.conversation = conversation;
                     document.querySelector('app-header').setAttribute('title', conversation.displayName);
-                    this.messageService.getMessages(true, conversationID, true);
+                    this.messageService.getMessages(true, this.conversationID);
                     this.headerService.title = conversation.displayName;
                     this.headerService.button = true;
                     if (conversation.anotherUserId) {
@@ -99,6 +100,12 @@ export class TalkingComponent implements OnInit, OnDestroy {
                         this.headerService.buttonIcon = `users`;
                         this.headerService.routerLink = `/group/${conversation.id}`;
                     }
+
+                    this.content = localStorage.getItem(String(this.conversationID));
+
+                    this.autoSaveInterval = setInterval(() => {
+                        localStorage.setItem(String(this.conversationID), this.content);
+                    }, 1000);
                 }
             });
         this.windowInnerHeight = window.innerHeight;
@@ -243,5 +250,7 @@ export class TalkingComponent implements OnInit, OnDestroy {
         this.messageService.resetVariables();
         this.colors = null;
         this.users = null;
+        this.conversationID = null;
+        this.autoSaveInterval = null;
     }
 }
