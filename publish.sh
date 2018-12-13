d1268adf18d607c127d8021887ea44d1f293c2b6
Anduin2017 <Anduin@aiursoft.com>
2018-12-14 01:46:13 +0800

    better publish.sh file

--- publish.sh
+++ publish.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# clean all built assets.
 rm -rvf ./www/
 rm -rvf ./dist/
 npm run prod-electron
-cp ./package.json ./www/
 
+# generate package.json without dependencies.
+cp ./package.json ./www/
 if [[ "$OSTYPE" == "darwin"* ]]
 then
     sed -i '' '6,$d' ./www/package.json
 else
     sed -i '6,$d' ./www/package.json
 fi
-
 echo '  "main": "index.js"' >> ./www/package.json
 echo '}' >> ./www/package.json
+
+# call electron builder without publishing anything.
 ./node_modules/.bin/electron-builder -p never
+
+# cp package.json so that release server could read version number.
+cp ./package.json ./dist/
+
+# rename built files to avoid space in file names.
 for f in dist/*\ *; do mv "$f" "${f// /.}"; done
