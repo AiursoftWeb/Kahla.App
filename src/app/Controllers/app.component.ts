6f20a80e0af3effc5db30e42437a3e550f99bf1e
xxyzz <elephantpolar@gmail.com>
2019-02-18 22:01:46 +0800

    only subscribe login user

--- src/app/Controllers/app.component.ts
+++ src/app/Controllers/app.component.ts
@@ -2,7 +2,6 @@ import { Component, HostListener } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { InitService } from '../Services/InitService';
 import Swal from 'sweetalert2';
-import { environment } from '../../environments/environment';
 
 @Component({
     selector: 'app-kahla',
@@ -35,35 +34,6 @@ export class AppComponent implements OnInit {
             if (Notification.permission === 'default') {
                 Notification.requestPermission();
             }
-
-            navigator.serviceWorker.ready.then(function(registration) {
-                registration.pushManager.getSubscription().then(function(sub) {
-                    if (sub === null) {
-                        registration.pushManager.subscribe({
-                            userVisibleOnly: true,
-                            applicationServerKey: urlBase64ToUint8Array(environment.applicationServerKey)
-                        }).then(function(pushSubscription) {
-                            // send to server
-                            console.log(pushSubscription);
-                        });
-                    }
-                });
-            });
-        }
-
-        function urlBase64ToUint8Array(base64String: string): Uint8Array {
-            const padding = '='.repeat((4 - base64String.length % 4) % 4);
-            const base64 = (base64String + padding)
-                .replace(/-/g, '+')
-                .replace(/_/g, '/');
-
-            const rawData = window.atob(base64);
-            const outputArray = new Uint8Array(rawData.length);
-
-            for (let i = 0; i < rawData.length; ++i) {
-                outputArray[i] = rawData.charCodeAt(i);
-            }
-            return outputArray;
         }
     }
 
