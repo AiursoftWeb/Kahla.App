adea2644696be0ab8e898c8aba5b5276dce458d0
Anduin2017 <anduin@aiursoft.com>
2019-02-24 17:36:15 +0800

    Open the public key to community.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   server: 'https://dev.server.kahla.app',
-  applicationServerKey: 'public application server key'
+  applicationServerKey: 'BJOTNmtE1V5GyDiYtoUzWbWLiaVMyfdnXFTvneJCbi9mwpw7FAVgQgIR9JN43TWkpT7ezBes1fPYr4nPqV6qXA0'
 };
