ed9736efa6d1d93311655e21127b553512029d7a
xxyzz <elephantpolar@gmail.com>
2019-04-07 16:12:29 +0800

    show common groups

--- src/app/Models/DiscoverUser.ts
+++ src/app/Models/DiscoverUser.ts
@@ -2,6 +2,7 @@ import { KahlaUser } from './KahlaUser';
 
 export class DiscoverUser {
     public commonFriends: number;
+    public commonGroups: number;
     public targetUser: KahlaUser;
     public sentRequest: boolean;
 }
