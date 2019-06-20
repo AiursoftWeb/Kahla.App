72aa0779598018db9b2b05a80cdec384c387d578
EdgeNeko <EdgeNeko@aiursoft.com>
2019-06-20 20:08:04 +0800

    feat: better `return` in widescreen

--- src/app/Controllers/header.component.ts
+++ src/app/Controllers/header.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from '../Services/MessageService';
 import { TimerService } from '../Services/TimerService';
+import { HomeService } from '../Services/HomeService';
 
 @Component({
     selector: 'app-header',
@@ -14,6 +15,7 @@ export class HeaderComponent {
 
     @Input() public title = 'Kahla';
     @Input() public returnButton = true;
+    @Input() public closeDirectly = false;
     @Input() public button = false;
     @Input() public buttonLink = '';
     @Input() public buttonIcon = '';
@@ -23,7 +25,9 @@ export class HeaderComponent {
     constructor(
         private router: Router,
         public timerService: TimerService,
-        public messageService: MessageService) {
+        public messageService: MessageService,
+        public homeService: HomeService,
+    ) {
             const userAgent = navigator.userAgent.toLowerCase();
             if (userAgent.includes('electron') && userAgent.includes('macintosh')) {
                 this.macOSElectron = true;
@@ -31,7 +35,7 @@ export class HeaderComponent {
     }
 
     public goBack(): void {
-        if (history.length === 1 || history.state.navigationId === 1) {
+        if (history.length === 1 || history.state.navigationId === 1 || (this.homeService.wideScreenEnabled && this.closeDirectly)) {
             this.router.navigate(['/home']);
         } else {
             history.back();
