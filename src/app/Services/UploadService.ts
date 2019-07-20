e5c8cefd1bab1e75b6f83502942d8d5af00ecf8a
xxyzz <elephantpolar@gmail.com>
2019-07-20 22:58:35 +0800

    fix firefox blocks cross-origin request bug

--- src/app/Services/UploadService.ts
+++ src/app/Services/UploadService.ts
@@ -269,7 +269,11 @@ export class UploadService {
         if (filekey !== -1 && !isNaN(filekey) && filekey !== 0) {
             this.filesApiService.GetFileURL(filekey).subscribe(response => {
                 if (response.code === 0) {
-                    window.location.href = response.downloadPath;
+                    const link = document.createElement('a');
+                    link.href = response.downloadPath;
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
                 }
             });
         }
