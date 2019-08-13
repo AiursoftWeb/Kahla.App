7a66b79f0510a0f1c6203ce25c586dbc69016f30
EdgeNeko <EdgeNeko@aiursoft.com>
2019-08-13 18:41:41 +0800

    feat: use new probe api

--- src/app/values.ts
+++ src/app/values.ts
@@ -1,5 +1,6 @@
 export class Values {
-    public static fileAddress = 'https://probe.aiursoft.com/Download/InSites/';
+    public static fileAddress = 'https://probe.aiursoft.com/Download/Open/';
+    public static fileDownloadAddress = 'https://probe.aiursoft.com/Download/File/';
     public static loadingImgURL = 'https://ui.cdn.aiursoft.com/images/loading.gif';
     public static sourceCodeURL = 'https://github.com/AiursoftWeb/Kahla.App/commit/';
 }
