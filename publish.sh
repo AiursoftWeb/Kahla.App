b15f7875610d9cfcc68b8875aea735814e6f5a51
Anduin Xue <anduin@aiursoft.com>
2019-09-27 22:46:12 +0800

    Use another way to build Kahla.

--- publish.sh
+++ publish.sh
@@ -17,7 +17,7 @@ echo '  "main": "index.js"' >> ./www/package.json
 echo '}' >> ./www/package.json
 
 # call electron builder without publishing anything.
-./node_modules/.bin/electron-builder -p never
+npx electron-builder -p never
 
 # cp package.json so that release server could read version number.
 cp ./package.json ./dist/
