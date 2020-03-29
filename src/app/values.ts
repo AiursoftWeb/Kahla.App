764fe553a4a2f30adc10f0ffd50babc924ce0496
Anduin2017 <anduin@aiursoft.com>
2020-03-29 12:57:37 +0800

    Fix wrong file path.

--- src/app/values.ts
+++ src/app/values.ts
@@ -1,5 +1,5 @@
 export class Values {
-    public static fileCompatAddress = 'https://probe.cdn.aiursoft.com/{site}';
+    public static fileCompatAddress = 'https://probe.cdn.aiursoft.com/{site}/';
     public static fileDownloadAddress = 'https://probe.download.cdn.aiursoft.com/';
     public static loadingImgURL = 'https://ui.cdn.aiursoft.com/images/loading.gif';
     public static sourceCodeURL = 'https://github.com/AiursoftWeb/Kahla.App/commit/';
