8a38a887ad0c228b3d5c62f7391ad4eb8953694b
Anduin Xue <anduin@aiursoft.com>
2020-05-01 16:44:50 +0800

    Use aiur.site domain.

--- src/app/values.ts
+++ src/app/values.ts
@@ -1,6 +1,6 @@
 export class Values {
-    public static fileCompatAddress = 'https://probe.aiursoft.com/download/open/{site}/';
-    public static fileDownloadAddress = 'https://probe.aiursoft.com/download/file/{site}/';
+    public static fileCompatAddress = 'https://{site}.aiur.site/';
+    public static fileDownloadAddress = 'https://{site}.download.aiur.site/';
     public static loadingImgURL = 'https://ui.cdn.aiursoft.com/images/loading.gif';
     public static sourceCodeURL = 'https://github.com/AiursoftWeb/Kahla.App/commit/';
 }
