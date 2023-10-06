4bb88cc7fdf359d46ca21f378bf687d727542146
Anduin Xue <anduin@aiursoft.com>
2023-10-06 11:07:27 +0000

    Migrate.

--- src/app/values.ts
+++ src/app/values.ts
@@ -1,4 +1,4 @@
 export class Values {
     public static loadingImgURL = 'https://ui.cdn.aiursoft.com/images/loading.svg';
-    public static sourceCodeURL = 'https://github.com/AiursoftWeb/Kahla.App/commit/';
+    public static sourceCodeURL = 'https://gitlab.aiursoft.cn/aiursoft/kahla.app/-/commit/';
 }
