170ee3de48f3ec7fa3107a3648b1f73418b70af2
EdgeNeko <edgeneko@aiursoft.com>
2024-09-24 18:13:29 +0800

    Add login code

--- src/app/Controllers/signin.component.ts
+++ src/app/Controllers/signin.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { ApiService } from "../Services/Api/ApiService";
 import { InitService } from "../Services/InitService";
 import Swal from "sweetalert2";
+import { AuthApiService } from "../Services/Api/AuthApiService";
+import { lastValueFrom } from "rxjs";
+import { HttpErrorResponse } from "@angular/common/http";
+import { AiurProtocal } from "../Models/AiurProtocal";
 
 @Component({
     templateUrl: "../Views/signin.html",
@@ -17,16 +21,24 @@ export class SignInComponent implements OnInit {
     constructor(
         public apiService: ApiService,
         public initService: InitService,
+        public authApiService: AuthApiService
     ) {}
 
     ngOnInit(): void {}
 
-    login() {
-        Swal.fire(
-            "Your login credential is",
-            `Username: ${this.userName} Password: ${this.password}`,
-            "success"
-        );
+    async login() {
+        try {
+            await lastValueFrom(this.authApiService.SignIn(this.userName, this.password));
+            this.initService.init();
+        } catch (err) {
+            if (err instanceof HttpErrorResponse) {
+                const error = err.error as AiurProtocal;
+                Swal.fire(error.message, "", "error");
+            } else {
+                Swal.fire("Unknown error", "", "error");
+            }
+        }
+        
     }
 
     register() {
