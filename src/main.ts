a6ea1e5acdab2838f23d5b68a6fcd4f4817d84d9
EdgeNeko <edgeneko@aiursoft.com>
2024-09-27 23:12:45 +0800

    Remove outdated polyfills

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import 'zone.js';
-
 if (environment.production) {
     enableProdMode();
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,12 +3,12 @@
  */
 import '@angular/localize/init';
 /** Evergreen browsers require these. **/
-import 'core-js/es/reflect';
-import smoothscroll from 'smoothscroll-polyfill';
+// import 'core-js/es/reflect';
+// import smoothscroll from 'smoothscroll-polyfill';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-// import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -47,7 +47,7 @@ import smoothscroll from 'smoothscroll-polyfill';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
-smoothscroll.polyfill();
+// smoothscroll.polyfill();
 /**
  * Required to support Web Animations `@angular/animation`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
