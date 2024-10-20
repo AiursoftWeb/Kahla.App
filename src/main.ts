2d3de3bc0f3e3e1a2aca209bf61d498774bbd280
EdgeNeko <edgeneko@aiursoft.com>
2024-10-20 18:53:09 +0800

    Make effects be shown on mobile devices.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+// Magic code for mobile devices to show hover effect correctly.
+// Reference: https://stackoverflow.com/questions/8330559/hover-effects-using-css3-touch-events
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+document.body.addEventListener('touchstart', () => {}, false);
+
 platformBrowserDynamic().bootstrapModule(AppModule);
