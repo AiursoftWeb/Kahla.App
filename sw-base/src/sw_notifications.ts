ad69450cec7631fd144425c857fdb3690799660e
EdgeNeko <edgeneko@aiursoft.com>
2024-12-06 23:33:18 +0800

    Adapt service worker to latest notifications

--- sw-base/src/sw_notifications.ts
+++ sw-base/src/sw_notifications.ts
@@ -1,84 +1,60 @@
 const sw = self as unknown as ServiceWorkerGlobalScope;
 
-sw.addEventListener('notificationclick', function (event) {
-    const data = event.notification.data;
+async function getWindowClients() {
+    const clients = (await sw.clients.matchAll()).filter(
+        t => t.type === 'window'
+    ) as WindowClient[];
+
+    return clients;
+}
+
+sw.addEventListener('notificationclick', event =>
     event.waitUntil(
-        sw.clients.matchAll().then(function (clientList) {
-            const wClientList = clientList.filter(t => t.type === 'window') as WindowClient[];
-            if (wClientList.length > 0) {
-                return wClientList[0].focus();
-            } else {
-                if (data && data.type === 0 && data.message.conversationID !== -1) {
-                    return sw.clients.openWindow(`/talking/${data.message.conversationId}`);
-                } else {
-                    return sw.clients.openWindow('/');
-                }
-            }
-        })
-    );
-    event.notification.close();
-});
+        (async () => {
+            const data = event.notification.data;
+            const clients = await getWindowClients();
+            if (clients.length) clients[0].focus();
+            else await sw.clients.openWindow(data.preferredUrl);
+            event.notification.close();
+        })()
+    )
+);
 
-sw.addEventListener('push', function (event) {
-    if (!event.data) {
-        return;
-    }
-    const data = event.data.json();
-    const imageLink = `https://probe.aiursoft.com/Download/Open/${encodeURIComponent(data.message.sender.iconFilePath).replace(/%2F/g, '/')}`;
+sw.addEventListener('push', event =>
+    event.waitUntil(
+        (async () => {
+            const data = event.data.json();
+            if (data.type === 0) {
+                // new message
+                const title =
+                    (data.mentioned ? '[Mentioned you] ' : '') + data.message.sender.nickName;
+                const message = data.message.preview;
 
-    if (data.type == 0 && !data.muted) {
-        // new message
-        const title = (data.mentioned ? '[Mentioned you] ' : '') + data.message.sender.nickName;
-        let message = data.message.content;
+                const bypassNotification = (await getWindowClients())
+                    .filter(t => t.focused)
+                    .some(t => t.url === `/talking/${data.message.threadId}`);
 
-        if (message.startsWith('[img]')) {
-            message = 'Photo';
-        } else if (message.startsWith('[video]')) {
-            message = 'Video';
-        } else if (message.startsWith('[file]')) {
-            message = 'File';
-        } else if (message.startsWith('[audio]')) {
-            message = 'Audio';
-        } else if (message.startsWith('[group]')) {
-            message = 'Group Invitation';
-        } else if (message.startsWith('[user]')) {
-            message = 'Contact card';
-        }
-
-        let showNotification = true;
-        event.waitUntil(
-            sw.clients.matchAll().then(function (clientList) {
-                clientList.forEach(function (client) {
-                    if (client.type !== 'window') return;
-                    const windowClient = client as WindowClient;
-                    const URLArray = windowClient.url.split('/');
-                    let URLId = -1;
-                    let talkingPage = false;
-                    if (URLArray.length > 4) {
-                        URLId = parseInt(URLArray[4]);
-                        if (URLArray[3] == 'talking') {
-                            talkingPage = true;
-                        }
-                    }
-                    if (
-                        !isNaN(URLId) &&
-                        URLId == data.message.conversationId &&
-                        windowClient.focused &&
-                        talkingPage
-                    ) {
-                        showNotification = false;
-                    }
+                if (bypassNotification) return;
+                await sw.registration.showNotification(title, {
+                    body: message,
+                    tag: data.message.threadId.toString(),
+                    data: {
+                        preferredUrl: `/talking/${data.message.threadId}`,
+                    },
                 });
+            } else if (data === 19) { // hard invited
+                const ownerNickname = data.thread.topTenMembers.find(t => t.isOwner)?.nickName;
+                const title = `You are invited to ${ownerNickname}'s thread`;
+                const message = data.thread.name;
 
-                if (showNotification) {
-                    return sw.registration.showNotification(title, {
-                        body: message,
-                        icon: imageLink,
-                        tag: data.threadId.toString(),
-                        data: data,
-                    });
-                }
-            })
-        );
-    }
-});
+                await sw.registration.showNotification(title, {
+                    body: message,
+                    tag: data.thread.id.toString(),
+                    data: {
+                        preferredUrl: `/talking/${data.thread.id}`,
+                    },
+                });
+            }
+        })()
+    )
+);
