9098343fe1f1b699cbfa2a9ff2afee7d542e86c8
EdgeNeko <edgeneko@aiursoft.com>
2024-12-07 20:00:48 +0800

    Order threads based on events

--- src/app/Repositories/MyThreadsOrderedRepository.ts
+++ src/app/Repositories/MyThreadsOrderedRepository.ts
@@ -1,8 +1,14 @@
-import { lastValueFrom } from 'rxjs';
+import { filter, lastValueFrom } from 'rxjs';
 import { ThreadInfoJoined } from '../Models/Threads/ThreadInfo';
 import { ThreadsApiService } from '../Services/Api/ThreadsApiService';
 import { RepositoryBase } from './RepositoryBase';
 import { Injectable } from '@angular/core';
+import { EventService } from '../Services/EventService';
+import { isThreadAddedEvent } from '../Models/Events/ThreadAddedEvent';
+import { isThreadRemovedEvent } from '../Models/Events/ThreadRemovedEvent';
+import { KahlaEventType } from '../Models/Events/EventType';
+import { NewMessageEvent } from '../Models/Events/NewMessageEvent';
+import { ThreadInfoCacheDictionary } from '../Caching/ThreadInfoCacheDictionary';
 
 @Injectable({
     providedIn: 'root',
@@ -15,12 +21,46 @@ export class MyThreadsOrderedRepository extends RepositoryBase<ThreadInfoJoined>
     };
     private _canLoadMore = true;
 
-    constructor(private threadsApiService: ThreadsApiService) {
+    constructor(
+        private threadsApiService: ThreadsApiService,
+        private eventService: EventService,
+        private threadInfoCacheDictionary: ThreadInfoCacheDictionary
+    ) {
         super();
+        eventService.onMessage.pipe(filter(t => isThreadAddedEvent(t))).subscribe(t => {
+            this.data = [t.thread, ...this.data];
+        });
+
+        eventService.onMessage.pipe(filter(t => isThreadRemovedEvent(t))).subscribe(t => {
+            this.data = this.data.filter(d => d.id !== t.threadId);
+        });
+
+        eventService.onMessage
+            .pipe(filter(t => t.type === KahlaEventType.NewMessage))
+            .subscribe(async t => {
+                const ev = t as NewMessageEvent;
+                const threadId = ev.message.threadId;
+                let thread: ThreadInfoJoined;
+                if (this.data.some(t => t.id === threadId)) {
+                    // Move the thread to the top
+                    thread = this.data.find(t => t.id === threadId)!;
+                    this.data = this.data.filter(t => t.id !== threadId);
+                } else {
+                    thread = await this.threadInfoCacheDictionary.get(threadId);
+                }
+                thread.messageContext.latestMessage = ev.message;
+                this.data = [thread, ...this.data];
+                this.threadInfoCacheDictionary.set(threadId, thread);
+            });
+    }
+
+    private updateCache(threads: ThreadInfoJoined[]) {
+        threads.forEach(t => this.threadInfoCacheDictionary.set(t.id, t));
     }
 
     protected async updateAllInternal(): Promise<void> {
         this.data = (await lastValueFrom(this.threadsApiService.Mine(20))).knownThreads;
+        this.updateCache(this.data);
         this._canLoadMore = this.data.length >= 20;
     }
 
@@ -31,6 +71,7 @@ export class MyThreadsOrderedRepository extends RepositoryBase<ThreadInfoJoined>
     protected async loadMoreInternal(take: number): Promise<void> {
         const lastId = this.data.at(-1)?.id;
         const newData = await lastValueFrom(this.threadsApiService.Mine(take, lastId));
+        this.updateCache(newData.knownThreads);
         // the data might be changed since then
         // find the lastId, and append the new data after it. Discard any items after the lastId
         this.data = [
